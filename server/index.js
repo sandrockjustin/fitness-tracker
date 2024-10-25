5d7ad2fd210c84a1ea74ca518ac0d5430292d28f
sandrockjustin <sandrock.justin@gmail.com>
2024-10-24 22:06:10 -0500

    server / expanding functionalities, imported database connection

--- server/index.js
+++ server/index.js
@@ -1,4 +1,7 @@
-const express = require('express'); // import Express with Node.js 'require' method
+import { User } from '../server/db/index';  // might be wrong to do this?
+const express = require('express');         // import Express with Node.js 'require' method
+const db = require('../server/db/index')
+
 const app = express();              // create Express instance named 'app'
 const port = 8080;                  // random port, can change as necessary
 
@@ -10,12 +13,80 @@ const port = 8080;                  // random port, can change as necessary
 //app.use(...)
 //app.use(...)
 
-//app.use(...)                      // apply google passport as authentication middleware
+//app.use(...)                      // apply google passport as auth middleware
 app.use(express.json())             // use express.json() as middleware
 /////////////////////////////////////////////////////////////////////////////////////
 
 app.use('/', express.static('client/dist'));  // on startup, serve files from webpack
 
-app.listen( port, () => {
-  console.log(`Express is listening on port ${port}...`)
+/* 
+  Undeveloped, but this will be for a more sensitive request (login) that is necessary
+  for authentication. I want to try to keep these separate so that we aren't unnecessarily
+  exposing a user's password or, even worse, somehow exposing the secret given to use by
+  Google Passport.
+*/
+app.get('/user', (req, res) => {
+})
+
+// Responsible for answering basic get request, return the current User's information from database
+app.get('/user/info', (req, res) => {
+
+  // Try to find a user matching the current user's ID
+  User.find({_id: req.body.id})
+    .then((foundUser) => {
+
+      // if no user with that ID is found
+      if (foundUser === null){
+        res.sendStatus(404)
+      }
+
+      // Why scrub user? Ensures sensitive information (password, salt) are not leaked
+      const scrubUser = {
+        _id: foundUser._id,
+        username: foundUser.username,
+        workouts: foundUser.workouts,
+        nutrition: foundUser.nutrition
+      }
+
+      // After user is scrubbed of sensitive info, deliver to client
+      res.status(200).send(scrubUser)
+    })
+    .catch((error) => {
+      console.error(`Error on GET request for /workouts.`)
+      res.sendStatus(500);
+    })
+})
+
+/* 
+  This is used to update ANY property for a user, this includes updates to workouts and nutrition...
+  
+  Patch request MUST include current client-side property that we are changing, this ensures that we
+  are adding to that array instead of totally replacing it.
+
+  Ex. req.data = {'workouts': [...this.state.user.workouts, newWorkout]}
+
+  If we don't do this, then the 'workouts' property will be completely overwritten by a workout
+*/
+app.put('user/info/:id', (req, res) => {
+
+  const filter = {_id: req.params.id};  // filter will be applied to the wildcard :id specified by Axios
+
+  const update = req.data;              // might be body rather than data, must double-check
+
+  User.findByIdAndUpdate(filter, update)  // attempt to find a user and update
+    .then((complete) => {
+      res.sendStatus(201);                // when completed, a 201 is sent
+    })
+    .catch((error) => {
+      console.error(`Error on post request to ${req.params.id}...`);
+      res.sendStatus(500);
+    })
+})
+
+// On successful connection to database, have our server begin listening for requests...
+db.on('open', () => {
+  app.listen( port, () => {
+    console.log(`Connection to database fitness-tracker established...`)
+    console.log(`Express is listening on port ${port}...`)
+  })
 })
