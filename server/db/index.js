ae7aab132f45538a9b23ff1f3c5365c968ec162b
sandrockjustin <sandrock.justin@gmail.com>
2024-10-24 20:01:45 -0500

    db / drafting Mongoose schema and connection

--- server/db/index.js
+++ server/db/index.js
@@ -1,3 +1,19 @@
-//establish connection to mongoose
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+// establish connection to mongoose
+// before we can do this, we need to declare a database name and a path
+// const db = mongoose.connect(() => {})
 
 //define schema
+const UserSchema = new Schema({
+  _id: Number, 
+  username: String,
+  workouts: Array,
+  nutrition: Array
+})
+
+// make model from Schema
+const User = mongoose.model('User', UserSchema);
+
+// we still need to export this and the connection to mongoose
