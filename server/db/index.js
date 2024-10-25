dcc45e9709ba5d58fa9cc0b88ba3ce574334083c
sandrockjustin <sandrock.justin@gmail.com>
2024-10-24 22:06:35 -0500

    db / attempt to export User model & database connection

--- server/db/index.js
+++ server/db/index.js
@@ -3,7 +3,7 @@ const { Schema } = mongoose;
 
 // establish connection to mongoose
 // before we can do this, we need to declare a database name and a path
-// const db = mongoose.connect(() => {})
+const db = mongoose.connect('mongodb://127.0.0.1:27017/fitness-tracker')
 
 //define schema
 const UserSchema = new Schema({
@@ -17,3 +17,7 @@ const UserSchema = new Schema({
 const User = mongoose.model('User', UserSchema);
 
 // we still need to export this and the connection to mongoose
+module.exports = {
+  db,
+  User
+}
