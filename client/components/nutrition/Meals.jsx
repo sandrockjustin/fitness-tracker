22f3055b70ff6dadd074a991d9406910ba4be059
benlongcp <164564032+benlongcp@users.noreply.github.com>
2024-10-25 16:11:46 -0500

    built out searchFood funcitonality

---
+++ client/components/nutrition/Meals.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react'
+
+export default function Meals(){
+  return(
+    <div>meals component</div>
+  )
+}
+//access saved workout list titles
+
+//access pantry list
+
+//pass a calories burned prop
+
+//create combination of foods to match the calories burned
