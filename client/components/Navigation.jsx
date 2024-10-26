fbfe298d4569ef18cd4c448d2f77685642e9cdc3
sandrockjustin <sandrock.justin@gmail.com>
2024-10-26 15:03:49 -0500

    feat / client - drafted logout button and functionality

--- client/components/Navigation.jsx
+++ client/components/Navigation.jsx
@@ -6,7 +6,8 @@ export default function Navigation(props) {
     <nav>
       <button type="button" onClick={(e) => props.updateView(e)}  name="WorkoutList">Current Workouts</button>
       <button type="button" onClick={(e) => props.updateView(e)}  name="WorkoutSearch">Search Workouts</button>
-      <button type="button" onClick={(e) => props.updateView(e)}  name="Nutrition">Nutrition Helper</button>						
+      <button type="button" onClick={(e) => props.updateView(e)}  name="Nutrition">Nutrition Helper</button>
+      <button type="button" onClick={(e) => props.updateView(e)}  name="Logout">Logout</button>									
     </nav>
   )
 
