e6e845b6bf5e8439c08f95381b08acb6cb4d986c
sandrockjustin <sandrock.justin@gmail.com>
2024-10-25 19:34:22 -0500

    fix / client - changed App.jsx so that we can switch between components

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,55 +1,107 @@
-import React, {useState, useEffect} from 'react'
+import React from 'react';
 import WorkoutSearch from './workouts/WorkoutSearch.jsx';
 import WorkoutList from './workouts/WorkoutList.jsx';
 import axios from 'axios';
-//importing everything into the App component
-export default function App(){
-  const [user, setUser] = useState(null);
-  const [workouts, setWorkouts] = useState([]);
 
-  //fetch user info
-  const fetchUser = () => {
-    axios.get(`/user/info/${'Jeremy Hernandez'}`)
-      .then((userData) => {
-        setUser(userData.data);
-        
-        // console.log('userData', userData);
-      })
-      .catch((err) => {
-        console.err('Failed to get userData');
-      })
-  }
-  //useEffect to fetch user info on start up
-  useEffect(() => {
-   fetchUser();
-    
-  }, []);
-  setTimeout(console.log, 5000, user);
-  return (
-    <div id="root-app">hello there world
-    {user ?
-      <div>
-      <WorkoutSearch />
-      <WorkoutList workouts={workouts}/>
-      </div>
-     :
-      <div>
-        <h1>Please Login</h1>
-      </div>
-      }
-    </div>
-  )
+class App extends React.Component {
+	constructor(props){
+		super(props);
+		
+		this.state = {
+			view: 'WorkoutList',
+			user: null, 
+			workouts: [],
+			nutrition: []
+		}
+
+		this.fetchUser = this.fetchUser.bind(this);
+		this.updateView = this.updateView.bind(this);
+	}
+
+	fetchUser() {
+		axios.get(`/user/info/${'Jeremy Hernandez'}`)
+		  .then((userData) => {
+			this.setState({user: userData.data});
+		  })
+		  .catch((err) => {
+			console.err('Failed to get userData');
+		  })
+	}
+	
+	updateView(e) {
+		this.setState({view: e.target.name})
+	}
+
+	componentDidMount(){
+		this.fetchUser();
+	}
+	
+	render(){
+		
+		switch(this.state.view){
+			case 'WorkoutList':
+				return (
+					<div id="root-app">Fitness Tracker
+					<nav>
+						<button type="button" onClick={(e) => this.updateView(e)}  name="WorkoutList">Current Workouts</button>
+						<button type="button" onClick={(e) => this.updateView(e)}  name="WorkoutSearch">Search Workouts</button>
+						<button type="button" onClick={(e) => this.updateView(e)}  name="Nutrition">Nutrition Helper</button>						
+					</nav>
+					{this.state.user ?
+					  <div>
+					  <WorkoutList workouts={this.state.workouts}/>
+					  </div>
+					 :
+					  <div>
+						<h1>Please Login</h1>
+					  </div>
+					  }
+					</div>				
+				)
+			case 'WorkoutSearch':
+				return (
+					<div id="root-app">Fitness Tracker
+					<nav>
+						<button type="button" onClick={(e) => this.updateView(e)}  name="WorkoutList">Current Workouts</button>
+						<button type="button" onClick={(e) => this.updateView(e)}  name="WorkoutSearch">Search Workouts</button>
+						<button type="button" onClick={(e) => this.updateView(e)}  name="Nutrition">Nutrition Helper</button>						
+					</nav>
+					{this.state.user ?
+					  <div>
+					  <WorkoutSearch/>
+					  </div>
+					 :
+					  <div>
+						<h1>Please Login</h1>
+					  </div>
+					  }
+					</div>
+				)
+			case 'Nutrition':
+				return (
+					<div id="root-app">Fitness Tracker
+					<nav>
+						<button type="button" onClick={(e) => this.updateView(e)} name="WorkoutList">Current Workouts</button>
+						<button type="button" onClick={(e) => this.updateView(e)}  name="WorkoutSearch">Search Workouts</button>
+						<button type="button" onClick={(e) => this.updateView(e)}  name="Nutrition">Nutrition Helper</button>						
+					</nav>
+						<div>NUTRITION component has not been implemented</div>
+					</div>
+				)
+			default:
+				return (
+					<div id="root-app">Fitness Tracker
+					<nav>
+						<button type="button" onClick={(e) => this.updateView(e)}  name="WorkoutList">Current Workouts</button>
+						<button type="button" onClick={(e) => this.updateView(e)}  name="WorkoutSearch">Search Workouts</button>
+						<button type="button" onClick={(e) => this.updateView(e)}  name="Nutrition">Nutrition Helper</button>						
+					</nav>
+						<div>LOGIN component has not been implemented.</div>
+					</div>
+				)
+		}
+	}
+	
 }
 
-
-
-//render login page view
-
-
-//render authenticated user view
-
-//app performs axios get request for user on useEffect
-
-//setState hook
-
-//refresh on state change
+export default App;
