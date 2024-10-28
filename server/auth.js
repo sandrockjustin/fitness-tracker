393b179f6877cb90cf596a1c839a9ee47c6b976d
sandrockjustin <sandrock.justin@gmail.com>
2024-10-28 16:50:41 -0500

    fix / server - authentication works for all routes, issue with redirect on logout

--- server/auth.js
+++ server/auth.js
@@ -8,7 +8,7 @@ dotenv.config();
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:8080/google/callback",
+    callbackURL: "http://localhost:8080/login-success",
     passReqToCallback: true
   },
   function(request, accessToken, refreshToken, profile, cb) {
