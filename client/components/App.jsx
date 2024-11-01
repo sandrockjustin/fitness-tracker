e2a6901bbeab7937c8717edd4c2c02d8bc0ffbfe
sandrockjustin <sandrock.justin@gmail.com>
2024-11-01 10:26:46 -0500

    Update App.jsx

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -21,7 +21,6 @@ export default function App() {
 	const [routines, setRoutines] = useState(null);
 
 	function fetchUser() {
-		console.log(new Date());
 		axios.get(`/user/info/`)
 		  .then((userData) => {
 				setUser(userData.data)
